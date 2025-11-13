4bb5572384728d0085fb6609599cb24c9344b9a9
alejandro gomez <henrya9202@hotmail.com>
2025-11-13 12:04:40 -0500

    Ejercicio 1: calcular total de la compra en el supermercado

---
+++ AlejandroGomez.js
@@ -0,0 +1,12 @@
+// Ejercicio 1: Calcular total de una compra en el supermercado
+
+let producto1 = Number(prompt("Ingrese el precio del primer producto:"));
+let producto2= Number(prompt("Ingrese el precio del segundo producto:"));
+let producto3= Number(prompt("Ingrese el precio del tercer producto:"));
+
+let total = producto1 + producto2 + producto3;
+
+Number.log("El total a pagar es: $" + total);
+
+
+
