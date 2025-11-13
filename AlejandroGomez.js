2cc91f449e1f43895f2c8110b235581b293d4828
alejandro gomez <henrya9202@hotmail.com>
2025-11-13 12:31:02 -0500

    se realizo la slucion del 3cer ejercicio del taller

--- AlejandroGomez.js
+++ AlejandroGomez.js
@@ -1,8 +1,8 @@
 // Ejercicio 1: Calcular total de una compra en el supermercado
 
 let producto1 = Number(prompt("Ingrese el precio del primer producto:"));
-let producto2= Number(prompt("Ingrese el precio del segundo producto:"));
-let producto3= Number(prompt("Ingrese el precio del tercer producto:"));
+let producto2 = Number(prompt("Ingrese el precio del segundo producto:"));
+let producto3 = Number(prompt("Ingrese el precio del tercer producto:"));
 
 let total = producto1 + producto2 + producto3;
 
@@ -18,3 +18,11 @@ let area = ancho * alto;
 console.log("El área del banner es de " + area + " metros cuadrados.");
 
 
+// Ejercicio 3: Calcular perímetro de un potrero rectangular
+
+let largo = parseFloat(prompt("Ingrese el largo del potrero en metros:"));
+let ancho = parseFloat(prompt("Ingrese el ancho del potrero en metros:"));
+
+let perimetro = 2 * (largo + ancho);
+
+console.log("El perímetro del potrero es de " + perimetro + " metros.");
